caf22a0f96c9d8b82b440e67cddccac4ec8acd54
AbuTalha <salim.meetme@gmail.com>
2022-07-13 00:13:49 +0300

    Initial Commit

---
+++ Persomi/PersomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersomiApp.swift
+//  Persomi
+//
+//  Created by AbuTalha on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PersomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersomiUITests/PersomiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PersomiUITestsLaunchTests.swift
+//  PersomiUITests
+//
+//  Created by AbuTalha on 13/07/2022.
+//
+
+import XCTest
+
+class PersomiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
